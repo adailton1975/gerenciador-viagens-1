d712d2ba84d4806367126029ae106a8909b7e1f5
adailton <adailton.1975@gmail.com>
2022-08-31 12:22:19 -0300

    Add outros testes

--- src/test/java/com/montanha/gerenciador/viagens/ViagensTest.java
+++ src/test/java/com/montanha/gerenciador/viagens/ViagensTest.java
@@ -1,6 +1,7 @@
 package com.montanha.gerenciador.viagens;
 
 import io.restassured.http.ContentType;
+import org.aspectj.weaver.patterns.IToken;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -47,4 +48,58 @@ public class ViagensTest {
 
 
     }
+    @Test
+    public void LogiUsrStatus201(){
+        baseURI = "http://localhost";
+        basePath = "/api";
+        port = 8089;
+        String token = given()
+            .body("{\n" +
+                    "  \"email\": \"admin@email.com\",\n" +
+                    "  \"senha\": \"654321\"\n" +
+                    "}")
+                .contentType(ContentType.JSON)
+            .when()
+                .post("/v1/auth")
+            .then()
+                .extract()
+                .path("data.token");
+        System.out.println(token);
+        given()
+                .header("Authorization", token)
+                .body("{\n" +
+                        "    \"acompanhante\": \"Suzi\",\n" +
+                        "    \"dataPartida\": \"2022-09-14\",\n" +
+                        "    \"dataRetorno\": \"2022-10-14\",\n" +
+                        "    \"localDeDestino\": \"Roraima\",\n" +
+                        "    \"regiao\": \"Norte\"\n" +
+                        "}").contentType(ContentType.JSON)
+                .when()
+                    .post("/v1/viagens")
+                .then().statusCode(201);
+    }
+    @Test
+    public void GetTodasViagens(){
+        baseURI = "http://localhost";
+        basePath = "/api";
+        port = 8089;
+        String token = given()
+                .body("{\n" +
+                        "  \"email\": \"usuario@email.com\",\n" +
+                        "  \"senha\": \"123456\"\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/v1/auth")
+                .then()
+                .extract()
+                .path("data.token");
+        System.out.println(token);
+    given()
+            .header("Authorization", token)
+            .when().get("/v1/viagens")
+            .then().statusCode(200);
+
+    }
+
 }
